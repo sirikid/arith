2248d8c3012cc1f6beb710d143f50bf70c29ed28
Ivan Sokolov <sirikid@openmailbox.org>
2016-12-18 01:54:56 +0300

    Removed redundant code

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,9 @@
 module Main where
 
 import Arith
-import Data.Bifunctor
-import Data.List
+import Data.Bifunctor (first)
+import Data.List (isPrefixOf)
 import System.IO
-
-data Failure = TokenizationF String | ParsingF [Token] | EvaluationF Term
-
-instance Show Failure where
-  show failure = case failure of
-    TokenizationF cause -> "can't tokenize " ++ show cause
-    ParsingF      cause -> "can't parse " ++ show cause
-    EvaluationF   cause -> "can't eval " ++ show cause
 
 main :: IO ()
 main = do
@@ -24,10 +16,8 @@ main = do
       if ":q" `isPrefixOf` line
         then return ()
         else do
-        putStrLn $ showEF $ return line >>= tokenize' >>= parse' >>= eval'
+        putStrLn $ either id show $ return line >>= tokenize' >>= parse' >>= eval'
         repl
-    showEF (Left failure) = "Failure: " ++ show failure
-    showEF (Right result) = " => " ++ show result
-    tokenize' = first TokenizationF . tokenize
-    parse'    = first ParsingF . parse
-    eval'     = first EvaluationF . eval
+    tokenize' = first (("Can't tokenize string "++) . show) . tokenize
+    parse'    = first (("Can't parse tokens "++) . show) . parse
+    eval'     = first (("Can't evaluate AST "++) . show) . eval
