b05309b0ec414260f14fe26bdc80ab2e3d2cb808
Ivan Sokolov <sirikid@openmailbox.org>
2016-12-18 11:53:45 +0300

    Implemented simple lexer

--- src/Arith/Lexer.hs
+++ src/Arith/Lexer.hs
@@ -3,8 +3,24 @@ module Arith.Lexer
   , tokenize
   ) where
 
+import Data.Maybe (fromMaybe)
+
 data Token = KwTrue | KwFalse | KwIf | KwThen | KwElse | KwZero | KwSucc | KwPred | KwIsZero
   deriving (Eq, Show)
 
 tokenize :: String -> Either String [Token]
-tokenize = Left
+tokenize input = fmap reverse $ foldl tryParseKw (Right []) $ words input where
+  tryParseKw acc word = case acc of
+    Left string -> Left $ string ++ " " ++ word
+    Right tokens -> fromMaybe (Left word) (fmap (Right . (:tokens)) (lookup word kwMap))
+  kwMap =
+    [("true"    ,KwTrue  )
+    ,("false"   ,KwFalse )
+    ,("if"      ,KwIf    )
+    ,("then"    ,KwThen  )
+    ,("else"    ,KwElse  )
+    ,("0"       ,KwZero  )
+    ,("succ"    ,KwSucc  )
+    ,("pred"    ,KwPred  )
+    ,("is_zero" ,KwIsZero)
+    ]
