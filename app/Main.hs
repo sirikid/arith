d632e2e7a2f459bcdc7ff9b61fd2881ebc510572
Ivan Sokolov <sirikid@openmailbox.org>
2016-12-22 00:11:17 +0300

    New REPL

--- app/Main.hs
+++ app/Main.hs
@@ -3,21 +3,24 @@ module Main where
 import Arith
 import Data.Bifunctor (first)
 import Data.List (isPrefixOf)
-import System.IO
+import System.IO (hFlush,stdout)
 
 main :: IO ()
 main = do
-  putStrLn "Type :q for quit"
-  repl where
-    repl = do
-      putStr " >> "
+  loop "Type :q for quit"
+  where
+    loop prev = do
+      putStr $ prev ++ "\n >> "
       hFlush stdout
       line <- getLine
       if ":q" `isPrefixOf` line
         then return ()
-        else do
-        putStrLn $ either id show $ return line >>= tokenize' >>= parse' >>= eval'
-        repl
+        else loop $ evalString line
+    evalString s = either id show $ do
+      tokens <- tokenize' s
+      ast    <- parse' tokens
+      result <- eval' ast
+      return result
     tokenize' = first (("Unexpected character sequence: "++) . show) . tokenize
     parse'    = first (("Unexpected token sequence: "++)     . show) . parse
     eval'     = first (("Unevaluable term: "++)              . show) . eval
