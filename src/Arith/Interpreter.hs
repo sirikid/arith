0585bdb00bca2c5d794a0b352d021dcb7f590f21
Ivan Sokolov <sirikid@openmailbox.org>
2016-12-21 11:41:35 +0300

    Added some predicates for terms

--- src/Arith/Interpreter.hs
+++ src/Arith/Interpreter.hs
@@ -1,8 +1,20 @@
 module Arith.Interpreter
   ( eval
+  , isValue
+  , isBooleanValue
+  , isNumericValue
   ) where
 
 import Arith.Parser (Term(..))
+
+isValue :: Term -> Bool
+isValue term = isBooleanValue term || isNumericValue term
+
+isBooleanValue :: Term -> Bool
+isBooleanValue _ = False
+
+isNumericValue :: Term -> Bool
+isNumericValue _ = False
 
 eval :: Term -> Either Term Term
 eval = Left
