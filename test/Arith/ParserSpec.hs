26e4d8ab4c2d96fff7d3690bb43670becf1eba5f
Ivan Sokolov <sirikid@openmailbox.org>
2016-12-28 19:19:06 +0300

    Simplify Parser specification

--- test/Arith/ParserSpec.hs
+++ test/Arith/ParserSpec.hs
@@ -2,11 +2,8 @@ module Arith.ParserSpec
   ( spec
   ) where
 
-import Test.Hspec
 import Arith.Parser
+import Test.Hspec
 
 spec :: Spec
-spec = do
-  describe "specification not written yet" $ do
-    it "does not test anything" $ do
-      pending
+spec = return ()
