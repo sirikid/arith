46184738519f92b9abcf9b3b18173da3b5683022
Ivan Sokolov <sirikid@openmailbox.org>
2017-01-24 06:34:06 +0300

    Backspace fixed, added history of expressions

--- app/Main.hs
+++ app/Main.hs
@@ -3,24 +3,20 @@ module Main where
 import Arith
 import Data.Bifunctor (first)
 import Data.List (isPrefixOf)
-import System.IO (hFlush,stdout)
+import System.Console.Readline (addHistory, readline)
 
 main :: IO ()
 main = do
-  go "Type :q for quit"
+  putStrLn "Type :q for quit"
+  go
   where
-    go prev = do
-      putStr $ prev ++ "\n >> "
-      hFlush stdout
-      line <- getLine
-      if ":q" `isPrefixOf` line
-        then return ()
-        else go $ evalString line
-    evalString s = either id show $ do
-      tokens <- tokenize' s
-      ast    <- parse' tokens
-      result <- eval' ast
-      return result
+    go = do
+      mbLine <- readline " >> "
+      case mbLine of
+        Nothing -> return ()
+        Just cmd | ":q" `isPrefixOf` cmd -> return ()
+        Just expr -> addHistory expr >> putStrLn (either id show $ compute expr) >> go
+    compute expr = tokenize' expr >>= parse' >>= eval'
     tokenize' = first (("Unexpected character sequence: "++) . show) . tokenize
     parse'    = first (("Unexpected token sequence: "++)     . show) . parse
     eval'     = first (("Unevaluable term: "++)              . show) . eval
