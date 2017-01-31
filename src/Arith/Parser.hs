5bcea722a84a453138396b82d18f2d3b0e9a6bc6
Ivan Sokolov <sirikid@openmailbox.org>
2017-01-31 14:01:50 +0300

    Implemented parser

--- src/Arith/Parser.hs
+++ src/Arith/Parser.hs
@@ -12,4 +12,32 @@ data Term = TmZero | TmTrue | TmFalse | TmSucc Term | TmPred Term | TmIsZero Ter
   deriving (Eq, Show)
 
 parse :: [Token] -> Either String Term
-parse = throwError . ("Unparsable token sequence: "++) . show
+parse tokens = do
+  (ts, tm) <- lookForExpression tokens
+  if null ts
+    then return tm
+    else throwError $ "Unutilized tokens: " ++ show ts
+
+lookUntil :: (Token -> Bool) -> [Token] -> Either String ([Token], Term)
+lookUntil predicate = go
+  where
+    go [] = throwError "Unexpected end of sequence"
+    go [EndOfExpression] = throwError "Unexpected end of expression"
+    go (KwZero:t:ts) | predicate t = return (ts, TmZero)
+    go (KwTrue:t:ts) | predicate t = return (ts, TmTrue)
+    go (KwFalse:t:ts) | predicate t = return (ts, TmFalse)
+    go (KwSucc:ts) = fmap TmSucc <$> go ts
+    go (KwPred:ts) = fmap TmPred <$> go ts
+    go (KwIsZero:ts) = fmap TmIsZero <$> go ts
+    go (KwIf:ts) = do
+      (ts, condition) <- lookForCondition ts
+      (ts, thenBranch) <- lookForThenBranch ts
+      (ts, elseBranch) <- lookForElseBranch ts
+      return (ts, TmIf condition thenBranch elseBranch)
+    go ts = throwError $ "Unparsable token sequence: " ++ show ts
+
+lookForExpression, lookForCondition, lookForThenBranch, lookForElseBranch :: [Token] -> Either String ([Token], Term)
+lookForExpression = lookUntil (== EndOfExpression)
+lookForCondition  = lookUntil (== KwThen)
+lookForThenBranch = lookUntil (== KwElse)
+lookForElseBranch = lookUntil (`elem` [KwIf, KwThen, KwElse, EndOfExpression])
--- test/Arith/ParserSpec.hs
+++ test/Arith/ParserSpec.hs
@@ -4,20 +4,34 @@ module Arith.ParserSpec
 
 import Arith.Lexer (Token(..))
 import Arith.Parser
-import Test.Hspec
+import SpecUtils (Spec, context, describe, it, shouldReturn, shouldThrow)
 
 spec :: Spec
 spec = do
   describe "parse" $ do
     it "builds abstract syntactic tree from the list of tokens" $ do
-      True `shouldBe` True
+      let tokens = [KwIf, KwIsZero, KwSucc, KwPred, KwZero, KwThen, KwTrue, KwElse, KwSucc, KwFalse, EndOfExpression]
+      let term = TmIf (TmIsZero (TmSucc (TmPred TmZero))) TmTrue (TmSucc TmFalse)
+      parse tokens `shouldReturn` term
 
     context "when an empty list given" $ do
-      it "fails" $ do
-        parse [] `shouldBe` Left []
+      it "fails with obscure error message" $ do
+        parse [] `shouldThrow` "Unexpected end of sequence"
 
-    context "when a constant given" $ do
-      it "returns corresponding term" $ do
-        parse [KwZero] `shouldBe` Right TmZero
-        parse [KwTrue] `shouldBe` Right TmTrue
-        parse [KwFalse] `shouldBe` Right TmFalse
+    context "when given a single EndOfExpression token" $ do
+      it "fails with a bit more informative message" $ do
+        parse [EndOfExpression] `shouldThrow` "Unexpected end of expression"
+
+    context "when given a token, that represents a value, and then EndOfExpression" $ do
+      it "returns corresponding value as a term" $ do
+        parse [KwZero, EndOfExpression] `shouldReturn` TmZero
+        parse [KwTrue, EndOfExpression] `shouldReturn` TmTrue
+        parse [KwFalse, EndOfExpression] `shouldReturn` TmFalse
+
+    context "when given a token, that represents an unary operator, and then any expression" $ do
+      it "returns corresponding unary operator applied to that expression" $ do
+        parse [KwSucc, KwZero, EndOfExpression] `shouldReturn` TmSucc TmZero
+        parse [KwPred, KwPred, KwTrue, EndOfExpression] `shouldReturn` TmPred (TmPred TmTrue)
+        parse [KwIsZero, KwSucc, KwIsZero, KwPred, KwFalse, EndOfExpression] `shouldReturn` TmIsZero (TmSucc (TmIsZero (TmPred TmFalse)))
+
+    -- TODO Specify "if-then-else" parsing
